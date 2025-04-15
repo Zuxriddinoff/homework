e608d34b7756be38ee299d5e278707ee5af29c53
DiyorMarufov <marufovdiyor9@gmail.com>
2025-04-15 16:54:49 +0500

    Question solved

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const a = 777
+
+// let str = String(a)
+
+// str = str.split("")
+
+// let sum = 0
+
+// for(let i of str){
+//     sum+=+i
+// }
+// const sum = [...String(a)].reduce((acc,val) => acc + +val,0)
+// console.log(sum);
+
+console.log([...String(a)].reduce((acc,val) => acc + +val,0))
+
+
+
+
+
+
+
