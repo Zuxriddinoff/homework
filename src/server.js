bf850a31112df71183ad294437e09193a60274a8
Zuxriddinoff <zuxriddinovoff@gmail.com>
2025-04-30 08:42:36 +0500

    Morgan

--- src/server.js
+++ src/server.js
@@ -3,14 +3,16 @@ import { config } from "dotenv"
 import cookieParser from "cookie-parser";
 import { connectionDB } from "./db/db.js";
 import adminRouter from './router/user.routes.js'
+import morgan from "morgan";
 config();
 
 const app = express()
 const PORT = +process.env.PORT;
 
-
 app.use(express.json())
 app.use(cookieParser())
+app.use(morgan('dev'))
+
 await connectionDB()
 
 app.use("/admin", adminRouter)
