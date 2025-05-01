f7a32a2cbf0bee2eaf3c3d13b279f4994e6501ae
DiyorMarufov <marufovdiyor9@gmail.com>
2025-05-01 09:18:26 +0500

    course route changed

--- src/router/course.route.js
+++ src/router/course.route.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
 
 import { CourseController } from "../controller/index.js";
+import { jwtAuthGuard, selfAuthGuard } from "../middleware/index.js";
 
 const router = Router();
 
 const controller = new CourseController();
 
-router.get("/courses", controller.getAll).post("/courses", controller.create);
+router
+  .get("/courses", jwtAuthGuard, selfAuthGuard, controller.getAll)
+  .post("/courses", jwtAuthGuard, selfAuthGuard, controller.create)
+  .put("/course/:id", jwtAuthGuard, selfAuthGuard, controller.updateCourseById)
+  .delete(
+    "/course/:id",
+    jwtAuthGuard,
+    selfAuthGuard,
+    controller.deleteCourseById
+  );
 
 export { router as courseRouter };
