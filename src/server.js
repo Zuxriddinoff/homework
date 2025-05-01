4f1880db024a7f5f74a5fa0eb0837aebddfe9184
Zuxriddinoff <zuxriddinovoff@gmail.com>
2025-05-01 14:58:55 +0500

    logger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from "express"
-import { config } from "dotenv"
-import cookieParser from "cookie-parser";
-import { connectionDB } from "./db/db.js";
 import adminRouter from './router/user.routes.js'
+import cookieParser from "cookie-parser";
+import fs from 'fs'
+import path from "path";
 import morgan from "morgan";
+
+import { connectionDB } from "./db/db.js";
+import { config } from "dotenv"
+import { memoryUsage } from "process";
+
 config();
 
 const app = express()
@@ -11,9 +16,22 @@ const PORT = +process.env.PORT;
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(morgan('dev'))
 
 await connectionDB()
+
+
+const __dirname = path.resolve();
+const filePath = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+    flag:'a'
+})
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(morgan('combined', {
+        stream:filePath
+    }))
+} else {
+    app.use(morgan('dev'))
+}
 
 app.use("/admin", adminRouter)
 
